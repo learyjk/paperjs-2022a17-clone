79043ee63128f028a40e23238a8515818eb9122c
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2023-02-10 09:49:53 -0800

    intersections and basic tween experimenting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 "use strict";
 import paperFull from "paper";
-import { Color, PathItem } from "paper/dist/paper-core";
+import { gsap } from "gsap";
+import { Color, CompoundPath, PathItem } from "paper/dist/paper-core";
 const { Path, Point } = paperFull;
 
 const init = () => {
@@ -13,46 +14,82 @@ const init = () => {
   let h = canvas.clientHeight;
 
   paperFull.setup(canvas);
-  paperFull.project.currentStyle = {
-    ...paperFull.project.currentStyle,
-    fillColor: new Color(0.16, 0.6, 0.97),
+
+  const twoTwoGroup = paperFull.project.importSVG(twoTwoEl);
+  twoTwoGroup.fillColor = new Color(1, 1, 1);
+  const twoThreeGroup = paperFull.project.importSVG(twoThreeEl);
+  twoThreeGroup.fillColor = new Color("transparent");
+  let twoThreeCompoundPath = twoThreeGroup.lastChild as paper.CompoundPath;
+  twoTwoGroup.fitBounds(paperFull.view.bounds);
+  twoThreeGroup.fitBounds(paperFull.view.bounds);
+
+  let circles = createCircles(w, h);
+
+  circles.forEach((circle) => {
+    colorIntersection(twoThreeCompoundPath, circle);
+  });
+
+  console.log(circles[0]);
+
+  let circleScales = circles.map((circle) => circle.scaling);
+
+  let tween = circles[29].tween(
+    { scaling: 0.0001 },
+    { scaling: 2 },
+    { duration: 1500 }
+  );
+  tween.onUpdate = function (event) {
+    colorIntersection(twoThreeCompoundPath, circles[29]);
   };
 
-  const twoTwo = paperFull.project.importSVG(twoTwoEl, {
-    expandedShpaes: true,
-  });
-  //   const twoThree = paper.project.importSVG(twoThreeEl, {
-  //     expandedShpaes: true,
+  // paperFull.view.onFrame = function () {
+  //   circles.forEach((circle) => {
+  //     colorIntersection(twoThreeCompoundPath, circle);
   //   });
-  let twoTwoPath = twoTwo.lastChild as paper.CompoundPath;
-
-  twoTwo.fillColor = null; //new Color(1, 1, 1);
-  twoTwo.strokeColor = new Color(0, 0, 0.0);
-  //   twoThree.fillColor = null; //new Color(0, 0, 0);
-  //   twoThree.strokeColor = new Color(0, 0, 0.0);
-  twoTwo.fitBounds(paperFull.view.bounds);
-  //   twoThree.fitBounds(paperFull.view.bounds);
-
-  let circle = new Path.Circle(new Point(w, 0), 300);
-  circle.fillColor = new Color(0, 0, 0.0);
-  circle.strokeColor = new Color(0, 0, 0.0);
-  let circle2 = new Path.Circle(new Point(w, 400), 300);
-  circle2.fillColor = null;
-  circle2.strokeColor = new Color(0, 0, 0.0);
-
-  showIntersections(circle, twoTwoPath);
+  // };
 };
 
-function showIntersections(path1, path2) {
-  var intersections = path2.getIntersections(path1);
-  console.log({ intersections });
-  for (var i = 0; i < intersections.length; i++) {
-    new Path.Circle({
-      center: intersections[i].point,
-      radius: 5,
-      fillColor: "#ffffff",
-    });
-  }
+function colorIntersection(path1: paper.CompoundPath, path2: paper.Path) {
+  let result = path2.intersect(path1);
+  result.fillColor = new Color(0, 0, 0);
 }
+
+function createCircles(w, h) {
+  let circles: paper.Path[] = [];
+  const numCols = 14;
+  let space = w / numCols;
+  for (let x = 0; x < w; x += space) {
+    for (let y = 0; y < h; y += space) {
+      let circle = new Path.Circle({
+        radius: space / 2,
+        center: new Point(x + space / 2, y + space / 2),
+        applyMatrix: false,
+        fillColor: new Color("#4698F8"),
+      });
+      circles.push(circle);
+    }
+  }
+  return circles;
+}
+
+// function showIntersections(
+//   path1: paper.CompoundPath,
+//   path2: paper.CompoundPath
+// ) {
+//   var intersections = path2.getIntersections(path1);
+//   let result = path1.subtract(path2);
+
+//   result.fillColor = new Color("#612A87");
+
+//   console.log({ intersections });
+//   intersections;
+//   for (var i = 0; i < intersections.length; i++) {
+//     new Path.Circle({
+//       center: intersections[i].point,
+//       radius: 5,
+//       fillColor: "#ffffff",
+//     });
+//   }
+// }
 
 document.addEventListener("DOMContentLoaded", init);
