530cab825e3fde4f33bcdf3e9deaaa574860d45c
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2023-02-10 16:49:59 -0800

    fin

--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,85 @@
 "use strict";
-import paperFull from "paper";
+import paperCore from "paper";
 import { gsap } from "gsap";
-import { Color, CompoundPath, PathItem } from "paper/dist/paper-core";
-const { Path, Point } = paperFull;
+
+const { Path, Point, Group, Color } = paperCore;
 
 const init = () => {
+  // Select elements we need from HTML
   const canvas = document.querySelector<HTMLCanvasElement>("#paper-canvas");
   const twoTwoEl = document.querySelector<SVGAElement>("#twotwo-svg");
-  const twoThreeEl = document.querySelector<SVGElement>("#twothree-svg");
+  const twoThreeEl = document.querySelector<SVGAElement>("#twothree-svg");
+
+  // exit our code if elements aren't found
   if (!canvas || !twoTwoEl || !twoThreeEl) return;
 
-  let w = canvas.clientWidth;
-  let h = canvas.clientHeight;
+  // initialize canvas and important values
+  paperCore.setup(canvas);
+  const w = canvas.clientWidth;
+  const h = canvas.clientHeight;
+  const NUM_COLS = 14;
 
-  paperFull.setup(canvas);
-
-  const twoTwoGroup = paperFull.project.importSVG(twoTwoEl);
+  // import white 2022 SVG
+  const twoTwoGroup = paperCore.project.importSVG(twoTwoEl);
   twoTwoGroup.fillColor = new Color(1, 1, 1);
-  const twoThreeGroup = paperFull.project.importSVG(twoThreeEl);
-  twoThreeGroup.fillColor = new Color("transparent");
-  let twoThreeCompoundPath = twoThreeGroup.lastChild as paper.CompoundPath;
-  twoTwoGroup.fitBounds(paperFull.view.bounds);
-  twoThreeGroup.fitBounds(paperFull.view.bounds);
 
-  let circles = createCircles(w, h);
+  // import black 2023 SVG
+  const twoThreeGroup = paperCore.project.importSVG(twoThreeEl);
+  twoThreeGroup.fillColor = new Color(0, 0, 0);
 
-  circles.forEach((circle) => {
-    colorIntersection(twoThreeCompoundPath, circle);
+  // create blue rectangle
+  const rect = new Path.Rectangle({
+    point: new Point(0, 0),
+    size: [w, h],
   });
+  rect.fillColor = new Color("#0D9AFF");
 
-  console.log(circles[0]);
+  // size SVGs to the viewport
+  twoTwoGroup.fitBounds(paperCore.view.bounds);
+  twoThreeGroup.fitBounds(paperCore.view.bounds);
 
-  let circleScales = circles.map((circle) => circle.scaling);
+  // group the blue rectangle and 2023 SVG together
+  const frontGroup = new Group([rect, ...twoThreeGroup.children]);
 
-  let tween = circles[29].tween(
-    { scaling: 0.0001 },
-    { scaling: 2 },
-    { duration: 1500 }
+  // create circlePaths, which will be used to create our clipping mask and animation
+  const circlePaths = createCircles(w, h, NUM_COLS);
+
+  // create a clipping mask from our circlePaths, blue rectangle, and 2023 SVG
+  const circleGroup = new Group([...circlePaths]);
+  let clipGroup = new Group([circleGroup, frontGroup]);
+  clipGroup.clipped = true;
+
+  gsap.fromTo(
+    circlePaths,
+    { scaling: 0.001 }, // don't use 0!
+    {
+      scaling: 2,
+      duration: 2,
+      repeat: -1,
+      yoyo: true,
+      stagger: 0.01,
+    }
   );
-  tween.onUpdate = function (event) {
-    colorIntersection(twoThreeCompoundPath, circles[29]);
-  };
-
-  // paperFull.view.onFrame = function () {
-  //   circles.forEach((circle) => {
-  //     colorIntersection(twoThreeCompoundPath, circle);
-  //   });
-  // };
 };
 
-function colorIntersection(path1: paper.CompoundPath, path2: paper.Path) {
-  let result = path2.intersect(path1);
-  result.fillColor = new Color(0, 0, 0);
-}
-
-function createCircles(w, h) {
+function createCircles(w, h, numCols) {
   let circles: paper.Path[] = [];
-  const numCols = 14;
+
+  // determine size of each circle
   let space = w / numCols;
+
+  // loop through width and height, incrementing by circle diameter each time.
   for (let x = 0; x < w; x += space) {
     for (let y = 0; y < h; y += space) {
       let circle = new Path.Circle({
         radius: space / 2,
         center: new Point(x + space / 2, y + space / 2),
-        applyMatrix: false,
-        fillColor: new Color("#4698F8"),
+        applyMatrix: false, // skip recalculations that we don't need.
       });
       circles.push(circle);
     }
   }
   return circles;
 }
-
-// function showIntersections(
-//   path1: paper.CompoundPath,
-//   path2: paper.CompoundPath
-// ) {
-//   var intersections = path2.getIntersections(path1);
-//   let result = path1.subtract(path2);
-
-//   result.fillColor = new Color("#612A87");
-
-//   console.log({ intersections });
-//   intersections;
-//   for (var i = 0; i < intersections.length; i++) {
-//     new Path.Circle({
-//       center: intersections[i].point,
-//       radius: 5,
-//       fillColor: "#ffffff",
-//     });
-//   }
-// }
 
 document.addEventListener("DOMContentLoaded", init);
